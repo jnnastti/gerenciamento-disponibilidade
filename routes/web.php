091c1fb75416a977696874449c86b3d863a2c2dc
Jannaina <jannaina.sangaletti@senior.com.br>
2024-05-11 21:29:27 -0300

    [CHANGE] Rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\ProfessionalAvailabilityController;
+use App\Http\Controllers\ProfessionalController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/professionals', [ProfessionalController::class, 'index']);
+Route::post('/professionals', [ProfessionalController::class, 'store']);
+Route::get('/professionals/{id}', [ProfessionalController::class, 'show']);
+Route::put('/professionals/{id}', [ProfessionalController::class, 'update']);
+Route::delete('/professionals/{id}', [ProfessionalController::class, 'destroy']);
+
+Route::get('/availability', [ProfessionalAvailabilityController::class, 'index']);
+Route::post('/availability', [ProfessionalAvailabilityController::class, 'store']);
+Route::get('/availability/{id}', [ProfessionalAvailabilityController::class, 'show']);
+Route::put('/availability/{id}', [ProfessionalAvailabilityController::class, 'update']);
+Route::delete('/availability/{id}', [ProfessionalAvailabilityController::class, 'destroy']);
+
